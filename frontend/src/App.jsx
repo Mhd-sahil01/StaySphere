07feb9b30e36a8458c8203965ab170d9f03a7613
muhammed sahil <muhammedsahil274@gmail.com>
2025-07-11 23:50:29 +0530

    set a basic footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
 
   return (
     <div data-theme="light">
       <Navbar />
+
+
+      {/* <Footer /> */}
     </div>
   )
 }
