e058343e93931998249c73859cf0d28760dc0e45
muhammed sahil <muhammedsahil274@gmail.com>
2025-07-26 11:34:21 +0530

    fix the ui of nabvar in mobile screen

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
                 <a className="text-lg font-bold whitespace-nowrap">StaySphere</a>
 
                 {/* Search bar  */}
-                <form className="search-bar hidden md:flex gap-1 mx-1 flex-1 max-w-md">
+                <form className="search-bar hidden md:flex justify-center gap-1 mx-1 flex-1 w-1/2 max-w-md">
                     <div className="form-control flex-1">
                         <input
                             type="text"
@@ -37,7 +37,6 @@ function Navbar() {
             </div>
 
             <div className="navbar-end flex-none">
-
                 {/* Mobile menu button - visible only on small screens */}
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle md:hidden">
@@ -57,6 +56,30 @@ function Navbar() {
                     <ul
                         tabIndex={0}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {/* Mobile search bar */}
+                        <li><form className="flex gap-1 w-full relative">
+                            <input
+                                type="text"
+                                placeholder="Locations..."
+                                className="input input-bordered flex-1 w-11/12"
+                            />
+                            <button type="submit" className="btn btn-ghost btn-circle absolute right-0">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                    />
+                                </svg>
+                            </button>
+                        </form>
+                        </li>
                         <li><Link to={"/"}>Explore</Link></li>
                         <li><Link to={"/new"}>Rent It Now</Link></li>
                         {
@@ -64,8 +87,12 @@ function Navbar() {
                                 <li><Link onClick={logout}><LogOut size={15}/>Logout</Link></li>
                             ) : (
                                 <>
-                                    <li><Link to={"/signup"}>Signup</Link></li>
-                                    <li><Link to={"/login"}>Login</Link></li>
+                                    <li>
+                                        <div className="flex gap-2">
+                                            <Link to={"/signup"} className="hover:text-green-400 font-bold">Signup</Link>
+                                            <Link to={"/login"} className="hover:text-green-400 font-bold">Login</Link>
+                                        </div>
+                                    </li>
                                 </>
                             )
                         }
@@ -87,7 +114,7 @@ function Navbar() {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Navbar;
